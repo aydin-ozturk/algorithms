a4acbd27b59e75f65b0a86d2ed731878e6269415
aydin.ozturk <ozt.aydin@gmail.com>
2022-10-29 00:00:28 -0400

    "Algorithms HW3 added"

---
+++ algorithms_HW_3.py
@@ -0,0 +1,20 @@
+# Below The Arithmetical Mean
+# When given a list, the program should return a list of all the elements below the original list’s arithmetical mean.
+# The arithmetical mean is the sum of all elements divided by the number of elements.
+# Example: [1, 3, 5, 6, 4, 10, 2, 3] (The arithmetical mean is 4.25), Return [1, 3, 4, 2, 3]
+
+def below_arithmetical_mean(array):
+    below = []
+    for i in array:
+        if i < sum(array) / len(array):
+            below.append(i)
+    return below
+
+
+# Two Lowest Elements
+# When given a list of elements, find the two lowest elements.
+# They can be equal to each other or different.
+# Example: [198, 3, 4, 9, 10, 9, 2], Return: 2, 3
+
+def two_lowest_elements(array):
+    return sorted(array)[:2]
